aae144c03ca3e989b5a7231ba09669e6f6d387f8
Arun <arunkannanak2007@gmail.com>
2026-05-26 16:03:13 +0530

    Services updated

--- src/com/wipro/plms/service/Services.java
+++ src/com/wipro/plms/service/Services.java
@@ -1,5 +1,26 @@
 package com.wipro.plms.service;
 
 public class Services {
-
+	private int VehicleNo;
+	private String name;
+	
+	public int getVehicleNo() {
+		return VehicleNo;
+	}
+	public void setVehicleNo(int vehicleNo) {
+		VehicleNo = vehicleNo;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return "Services [VehicleNo=" + VehicleNo + ", name=" + name + "]";
+	}
+	
+	
 }
