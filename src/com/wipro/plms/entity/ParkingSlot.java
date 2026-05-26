4e210e7dd5419e28f5bcd6d0c825b8a9fd7aab38
karthikeyan06092006 <b.karthikeyan1000@gmail.com>
2026-05-26 16:00:26 +0530

    entity complete

---
+++ src/com/wipro/plms/entity/ParkingSlot.java
@@ -0,0 +1,28 @@
+package com.wipro.plms.entity;
+
+public class ParkingSlot {
+	private String slotId;
+	private boolean occupied;
+	private String vehicleNumber;
+	
+	public String getSlotId() {
+		return slotId;
+	}
+	public void setSlotId(String slotId) {
+		this.slotId = slotId;
+	}
+	public boolean isOccupied() {
+		return occupied;
+	}
+	public void setOccupied(boolean occupied) {
+		this.occupied = occupied;
+	}
+	public String getVehicleNumber() {
+		return vehicleNumber;
+	}
+	public void setVehicleNumber(String vehicleNumber) {
+		this.vehicleNumber = vehicleNumber;
+	}
+	
+	
+}
--- src/com/wipro/plms/entity/Vehicle.java
+++ src/com/wipro/plms/entity/Vehicle.java
@@ -1,5 +1,20 @@
 package com.wipro.plms.entity;
 
 public class Vehicle {
-	private int id;
+	private String vehicleNumber;
+	private String vehicleType;
+	
+	public String getVehicleNumber() {
+		return vehicleNumber;
+	}
+	public void setVehicleNumber(String vehicleNumber) {
+		this.vehicleNumber = vehicleNumber;
+	}
+	public String getVehicleType() {
+		return vehicleType;
+	}
+	public void setVehicleType(String vehicleType) {
+		this.vehicleType = vehicleType;
+	}
+	
 }
